3b87e2d5a24bdb8aa715a2e4a526c578bd8b7355
Vikas Kumar <sakiv.sherawat@gmail.com>
2017-04-12 04:12:19 +0100

    Updating buying fooditem, profile and history of user

--- main/Customer/views.py
+++ main/Customer/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from django.contrib import messages
+
 # Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from ..models import Shop, User, Customer, FoodItem
@@ -14,15 +16,15 @@ def fooditems(request, shop_id):
 def buy_fooditem(request, fooditem_id):
     if not request.user.is_authenticated:
         print("user not authenticated")
+        messages.add_message(request, messages.INFO, 'You must be logged in to place an order')
         return redirect('home')
     else:
         fooditem = get_object_or_404(FoodItem, pk=fooditem_id)
-        price = fooditem.price
         current_user = get_object_or_404(Customer, user_id=request.user.id)
-        fooditem.shop.shop_owner.credit += price
+
+        fooditem.shop.shop_owner.credit += fooditem.price
         fooditem.shop.shop_owner.save()
-        current_user.balance -= price
+
+        current_user.balance -= fooditem.price
         current_user.save()
-        print(current_user.balance)
-        print(fooditem.shop.shop_owner.credit)
-        return render(request, "main/thankyou.html")
+        return render(request, "main/order_confirmation.html", {'fooditem': fooditem, 'user': current_user})
