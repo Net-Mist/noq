0338ae3428c5b26989927c4c49c3cfe54ec7d125
Sebastien Iooss <sebastien.iooss@student.ecp.fr>
2017-04-12 21:21:11 +0800

    PostgreSQL

--- noq/settings.py
+++ noq/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'noq.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'compose',
+        'USER': 'admin',
+        'PASSWORD': 'SIVGXEXXRCAXPDME',
+        'HOST': 'sl-aus-syd-1-portal.3.dblayer.com',
+        'PORT': '15811',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
